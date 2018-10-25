cb1bb9d6bc86ffb36af84b0498496ed05693c9a3
Suh Yoon <suh@scripted.org>
2018-10-25 19:51:17 +0000

    final

--- js/script.js
+++ js/script.js
@@ -8,4 +8,47 @@
 
 
 // DOCUMENT READY FUNCTION BELOW
+var userChoice;
+var computerChoice;
+var winner; 
+$("#shoot").click(function(){
+     var userChoice= $("#input").val();
+     computerChoice=Math.random();
+     $("#userChoice").text(userChoice);
 
+     
+    console.log(computerChoice);
+     if(computerChoice <0.999 && computerChoice>0.666) {
+         computerChoice="rock"
+        $("#computerChoice").text("ROCK"); 
+     }else if(computerChoice<.666 && computerChoice>0.333){
+        $("#computerChoice").text("SCISSORS");
+         computerChoice="scissors"
+    }else if(computerChoice<0.333 && computerChoice>0){
+        $("#computerChoice").text("PAPER")
+         computerChoice="paper"
+    }
+
+    if(userChoice===computerChoice){ 
+        winner = "TIE";
+        $("#result").text(winner);
+    }else if(userChoice==="scissors" && computerChoice==="rock"){
+        $("#result").text("COMPUTER WINS");
+    }else if(userChoice==="rock" && computerChoice==="paper"){
+        $("#result").text("COMPUTER WINS");
+    }else if(userChoice==="paper" && computerChoice==="rock"){
+        $("#result").text("COMPUTER WINS");
+    }else if(userChoice==="rock" && computerChoice==="scissors"){
+        $("#result").text("YOU WIN");
+    }else if(userChoice==="scissors" && computerChoice==="paper"){
+        $("#result").text("YOU WIN"); 
+    }else if(userChoice==="paper" && computerChoice==="rock"){
+        $("#result").text("YOU WIN");
+    }else if(userChoice==="scissors" && computerChoice==="paper"){
+        $("#result").text("YOU WIN"); 
+        
+    }
+    }); 
+    
+    console.log("#result"); 
+    
